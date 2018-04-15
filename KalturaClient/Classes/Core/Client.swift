f5c38fff466747dafc0c1b024db69e467a4c23ab
Jenkins <jenkins@kaltura.com>
2018-04-15 12:06:25 +0000

    Auto-generated by Jenkins

--- KalturaClient/Classes/Core/Client.swift
+++ KalturaClient/Classes/Core/Client.swift
@@ -8,7 +8,7 @@
 // to do with audio, video, and animation what Wiki platfroms allow them to do with
 // text.
 //
-// Copyright (C) 2006-2017  Kaltura Inc.
+// Copyright (C) 2006-2018  Kaltura Inc.
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
@@ -40,8 +40,8 @@
 		
 		super.init()
 		
-		clientTag = "swift:18-01-21"
-		apiVersion = "4.3.25.19318"
+		clientTag = "swift:18-04-15"
+		apiVersion = "4.3.25.21736"
 	}
 }
 
