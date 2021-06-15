75c90ae7447069bf66d4c2310c5a2ec920d05e44
Backend CI <ott.rnd.core@kaltura.com>
2021-06-15 14:31:52 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs-ios-and-android/269, branch 6_5_1

---
+++ KalturaClient/Classes/Services/InboxMessageService.swift
@@ -0,0 +1,106 @@
+// ===================================================================================================
+//                           _  __     _ _
+//                          | |/ /__ _| | |_ _  _ _ _ __ _
+//                          | ' </ _` | |  _| || | '_/ _` |
+//                          |_|\_\__,_|_|\__|\_,_|_| \__,_|
+//
+// This file is part of the Kaltura Collaborative Media Suite which allows users
+// to do with audio, video, and animation what Wiki platfroms allow them to do with
+// text.
+//
+// Copyright (C) 2006-2020  Kaltura Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// @ignore
+// ===================================================================================================
+
+/**
+ * This class was generated using exec.php
+ * against an XML schema provided by Kaltura.
+ * 
+ * MANUAL CHANGES TO THIS CLASS WILL BE OVERWRITTEN.
+ */
+
+public final class InboxMessageService{
+
+	public class GetTokenizer: ClientTokenizer  {
+		
+		public var id: BaseTokenizedObject {
+			get {
+				return self.append("id") 
+			}
+		}
+	}
+
+	/**  TBD  */
+	public static func get(id: String) -> RequestBuilder<InboxMessage, InboxMessage.InboxMessageTokenizer, GetTokenizer> {
+		let request: RequestBuilder<InboxMessage, InboxMessage.InboxMessageTokenizer, GetTokenizer> = RequestBuilder<InboxMessage, InboxMessage.InboxMessageTokenizer, GetTokenizer>(service: "inboxmessage", action: "get")
+			.setParam(key: "id", value: id)
+
+		return request
+	}
+
+	public class ListTokenizer: ClientTokenizer  {
+		
+		public func filter<T: InboxMessageFilter.InboxMessageFilterTokenizer>() -> T {
+			return T(self.append("filter"))
+		}
+		
+		public func pager<T: FilterPager.FilterPagerTokenizer>() -> T {
+			return T(self.append("pager"))
+		}
+	}
+
+	public static func list() -> RequestBuilder<InboxMessageListResponse, InboxMessageListResponse.InboxMessageListResponseTokenizer, ListTokenizer> {
+		return list(filter: nil)
+	}
+
+	public static func list(filter: InboxMessageFilter?) -> RequestBuilder<InboxMessageListResponse, InboxMessageListResponse.InboxMessageListResponseTokenizer, ListTokenizer> {
+		return list(filter: filter, pager: nil)
+	}
+
+	/**  List inbox messages  */
+	public static func list(filter: InboxMessageFilter?, pager: FilterPager?) -> RequestBuilder<InboxMessageListResponse, InboxMessageListResponse.InboxMessageListResponseTokenizer, ListTokenizer> {
+		let request: RequestBuilder<InboxMessageListResponse, InboxMessageListResponse.InboxMessageListResponseTokenizer, ListTokenizer> = RequestBuilder<InboxMessageListResponse, InboxMessageListResponse.InboxMessageListResponseTokenizer, ListTokenizer>(service: "inboxmessage", action: "list")
+			.setParam(key: "filter", value: filter)
+			.setParam(key: "pager", value: pager)
+
+		return request
+	}
+
+	public class UpdateStatusTokenizer: ClientTokenizer  {
+		
+		public var id: BaseTokenizedObject {
+			get {
+				return self.append("id") 
+			}
+		}
+		
+		public var status: BaseTokenizedObject {
+			get {
+				return self.append("status") 
+			}
+		}
+	}
+
+	/**  Updates the message status.  */
+	public static func updateStatus(id: String, status: InboxMessageStatus) -> RequestBuilder<Bool, BaseTokenizedObject, UpdateStatusTokenizer> {
+		let request: RequestBuilder<Bool, BaseTokenizedObject, UpdateStatusTokenizer> = RequestBuilder<Bool, BaseTokenizedObject, UpdateStatusTokenizer>(service: "inboxmessage", action: "updateStatus")
+			.setParam(key: "id", value: id)
+			.setParam(key: "status", value: status.rawValue)
+
+		return request
+	}
+}
---
+++ KalturaClient/Classes/Services/PartnerService.swift
@@ -0,0 +1,86 @@
+// ===================================================================================================
+//                           _  __     _ _
+//                          | |/ /__ _| | |_ _  _ _ _ __ _
+//                          | ' </ _` | |  _| || | '_/ _` |
+//                          |_|\_\__,_|_|\__|\_,_|_| \__,_|
+//
+// This file is part of the Kaltura Collaborative Media Suite which allows users
+// to do with audio, video, and animation what Wiki platfroms allow them to do with
+// text.
+//
+// Copyright (C) 2006-2020  Kaltura Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// @ignore
+// ===================================================================================================
+
+/**
+ * This class was generated using exec.php
+ * against an XML schema provided by Kaltura.
+ * 
+ * MANUAL CHANGES TO THIS CLASS WILL BE OVERWRITTEN.
+ */
+
+public final class PartnerService{
+
+	public class AddTokenizer: ClientTokenizer  {
+		
+		public func partner<T: Partner.PartnerTokenizer>() -> T {
+			return T(self.append("partner"))
+		}
+		
+		public func partnerSetup<T: PartnerSetup.PartnerSetupTokenizer>() -> T {
+			return T(self.append("partnerSetup"))
+		}
+	}
+
+	/**  Add a partner with default user  */
+	public static func add(partner: Partner, partnerSetup: PartnerSetup) -> RequestBuilder<Partner, Partner.PartnerTokenizer, AddTokenizer> {
+		let request: RequestBuilder<Partner, Partner.PartnerTokenizer, AddTokenizer> = RequestBuilder<Partner, Partner.PartnerTokenizer, AddTokenizer>(service: "partner", action: "add")
+			.setParam(key: "partner", value: partner)
+			.setParam(key: "partnerSetup", value: partnerSetup)
+
+		return request
+	}
+
+	public class ExternalLoginTokenizer: ClientTokenizer  {
+	}
+
+	/**  Returns a login session for external system (like OVP)  */
+	public static func externalLogin() -> RequestBuilder<LoginSession, LoginSession.LoginSessionTokenizer, ExternalLoginTokenizer> {
+		let request: RequestBuilder<LoginSession, LoginSession.LoginSessionTokenizer, ExternalLoginTokenizer> = RequestBuilder<LoginSession, LoginSession.LoginSessionTokenizer, ExternalLoginTokenizer>(service: "partner", action: "externalLogin")
+
+		return request
+	}
+
+	public class ListTokenizer: ClientTokenizer  {
+		
+		public func filter<T: PartnerFilter.PartnerFilterTokenizer>() -> T {
+			return T(self.append("filter"))
+		}
+	}
+
+	public static func list() -> RequestBuilder<PartnerListResponse, PartnerListResponse.PartnerListResponseTokenizer, ListTokenizer> {
+		return list(filter: nil)
+	}
+
+	/**  Internal API !!! Returns the list of active Partners  */
+	public static func list(filter: PartnerFilter?) -> RequestBuilder<PartnerListResponse, PartnerListResponse.PartnerListResponseTokenizer, ListTokenizer> {
+		let request: RequestBuilder<PartnerListResponse, PartnerListResponse.PartnerListResponseTokenizer, ListTokenizer> = RequestBuilder<PartnerListResponse, PartnerListResponse.PartnerListResponseTokenizer, ListTokenizer>(service: "partner", action: "list")
+			.setParam(key: "filter", value: filter)
+
+		return request
+	}
+}
