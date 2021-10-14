ef8603631d791d4b09226ac155ee0a9d3fb3eb13
Backend CI <ott.rnd.core@kaltura.com>
2021-10-14 15:00:13 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs-ios-and-android/354, branch 6_8_1_1

---
+++ KalturaClient/Classes/Model/Objects/LicensedUrl.swift
@@ -0,0 +1,90 @@
+// ===================================================================================================
+//                           _  __     _ _
+//                          | |/ /__ _| | |_ _  _ _ _ __ _
+//                          | ' </ _` | |  _| || | '_/ _` |
+//                          |_|\_\__,_|_|\__|\_,_|_| \__,_|
+//
+// This file is part of the Kaltura Collaborative Media Suite which allows users
+// to do with audio, video, and animation what Wiki platfroms allow them to do with
+// text.
+//
+// Copyright (C) 2006-2020  Kaltura Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// @ignore
+// ===================================================================================================
+
+/**
+ * This class was generated using exec.php
+ * against an XML schema provided by Kaltura.
+ * 
+ * MANUAL CHANGES TO THIS CLASS WILL BE OVERWRITTEN.
+ */
+
+open class LicensedUrl: ObjectBase {
+
+	public class LicensedUrlTokenizer: ObjectBase.ObjectBaseTokenizer {
+		
+		public var mainUrl: BaseTokenizedObject {
+			get {
+				return self.append("mainUrl") 
+			}
+		}
+		
+		public var altUrl: BaseTokenizedObject {
+			get {
+				return self.append("altUrl") 
+			}
+		}
+	}
+
+	/**  Main licensed URL  */
+	public var mainUrl: String? = nil
+	/**  An alternate URL to use in case the main fails  */
+	public var altUrl: String? = nil
+
+
+	public func setMultiRequestToken(mainUrl: String) {
+		self.dict["mainUrl"] = mainUrl
+	}
+	
+	public func setMultiRequestToken(altUrl: String) {
+		self.dict["altUrl"] = altUrl
+	}
+	
+	public override func populate(_ dict: [String: Any]) throws {
+		try super.populate(dict);
+		// set members values:
+		if dict["mainUrl"] != nil {
+			mainUrl = dict["mainUrl"] as? String
+		}
+		if dict["altUrl"] != nil {
+			altUrl = dict["altUrl"] as? String
+		}
+
+	}
+
+	internal override func toDictionary() -> [String: Any] {
+		var dict: [String: Any] = super.toDictionary()
+		if(mainUrl != nil) {
+			dict["mainUrl"] = mainUrl!
+		}
+		if(altUrl != nil) {
+			dict["altUrl"] = altUrl!
+		}
+		return dict
+	}
+}
+
---
+++ KalturaClient/Classes/Services/SubscriptionService.swift
@@ -0,0 +1,143 @@
+// ===================================================================================================
+//                           _  __     _ _
+//                          | |/ /__ _| | |_ _  _ _ _ __ _
+//                          | ' </ _` | |  _| || | '_/ _` |
+//                          |_|\_\__,_|_|\__|\_,_|_| \__,_|
+//
+// This file is part of the Kaltura Collaborative Media Suite which allows users
+// to do with audio, video, and animation what Wiki platfroms allow them to do with
+// text.
+//
+// Copyright (C) 2006-2020  Kaltura Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// @ignore
+// ===================================================================================================
+
+/**
+ * This class was generated using exec.php
+ * against an XML schema provided by Kaltura.
+ * 
+ * MANUAL CHANGES TO THIS CLASS WILL BE OVERWRITTEN.
+ */
+
+public final class SubscriptionService{
+
+	public class AddTokenizer: ClientTokenizer  {
+		
+		public func subscription<T: Subscription.SubscriptionTokenizer>() -> T {
+			return T(self.append("subscription"))
+		}
+	}
+
+	/**  Internal API !!! Insert new subscription for partner  */
+	public static func add(subscription: Subscription) -> RequestBuilder<Subscription, Subscription.SubscriptionTokenizer, AddTokenizer> {
+		let request: RequestBuilder<Subscription, Subscription.SubscriptionTokenizer, AddTokenizer> = RequestBuilder<Subscription, Subscription.SubscriptionTokenizer, AddTokenizer>(service: "subscription", action: "add")
+			.setParam(key: "subscription", value: subscription)
+
+		return request
+	}
+
+	public class DeleteTokenizer: ClientTokenizer  {
+		
+		public var id: BaseTokenizedObject {
+			get {
+				return self.append("id") 
+			}
+		}
+	}
+
+	/**  Internal API !!! Delete subscription  */
+	public static func delete(id: Int64) -> RequestBuilder<Bool, BaseTokenizedObject, DeleteTokenizer> {
+		let request: RequestBuilder<Bool, BaseTokenizedObject, DeleteTokenizer> = RequestBuilder<Bool, BaseTokenizedObject, DeleteTokenizer>(service: "subscription", action: "delete")
+			.setParam(key: "id", value: id)
+
+		return request
+	}
+
+	public class ListTokenizer: ClientTokenizer  {
+		
+		public func filter<T: SubscriptionFilter.SubscriptionFilterTokenizer>() -> T {
+			return T(self.append("filter"))
+		}
+		
+		public func pager<T: FilterPager.FilterPagerTokenizer>() -> T {
+			return T(self.append("pager"))
+		}
+	}
+
+	public static func list() -> RequestBuilder<SubscriptionListResponse, SubscriptionListResponse.SubscriptionListResponseTokenizer, ListTokenizer> {
+		return list(filter: nil)
+	}
+
+	public static func list(filter: SubscriptionFilter?) -> RequestBuilder<SubscriptionListResponse, SubscriptionListResponse.SubscriptionListResponseTokenizer, ListTokenizer> {
+		return list(filter: filter, pager: nil)
+	}
+
+	/**  Returns a list of subscriptions requested by Subscription ID or file ID  */
+	public static func list(filter: SubscriptionFilter?, pager: FilterPager?) -> RequestBuilder<SubscriptionListResponse, SubscriptionListResponse.SubscriptionListResponseTokenizer, ListTokenizer> {
+		let request: RequestBuilder<SubscriptionListResponse, SubscriptionListResponse.SubscriptionListResponseTokenizer, ListTokenizer> = RequestBuilder<SubscriptionListResponse, SubscriptionListResponse.SubscriptionListResponseTokenizer, ListTokenizer>(service: "subscription", action: "list")
+			.setParam(key: "filter", value: filter)
+			.setParam(key: "pager", value: pager)
+
+		return request
+	}
+
+	public class UpdateTokenizer: ClientTokenizer  {
+		
+		public var id: BaseTokenizedObject {
+			get {
+				return self.append("id") 
+			}
+		}
+		
+		public func subscription<T: Subscription.SubscriptionTokenizer>() -> T {
+			return T(self.append("subscription"))
+		}
+	}
+
+	/**  Update Subscription  */
+	public static func update(id: Int64, subscription: Subscription) -> RequestBuilder<Subscription, Subscription.SubscriptionTokenizer, UpdateTokenizer> {
+		let request: RequestBuilder<Subscription, Subscription.SubscriptionTokenizer, UpdateTokenizer> = RequestBuilder<Subscription, Subscription.SubscriptionTokenizer, UpdateTokenizer>(service: "subscription", action: "update")
+			.setParam(key: "id", value: id)
+			.setParam(key: "subscription", value: subscription)
+
+		return request
+	}
+
+	public class ValidateCouponTokenizer: ClientTokenizer  {
+		
+		public var id: BaseTokenizedObject {
+			get {
+				return self.append("id") 
+			}
+		}
+		
+		public var code: BaseTokenizedObject {
+			get {
+				return self.append("code") 
+			}
+		}
+	}
+
+	/**  Returns information about a coupon for subscription  */
+	public static func validateCoupon(id: Int, code: String) -> RequestBuilder<Coupon, Coupon.CouponTokenizer, ValidateCouponTokenizer> {
+		let request: RequestBuilder<Coupon, Coupon.CouponTokenizer, ValidateCouponTokenizer> = RequestBuilder<Coupon, Coupon.CouponTokenizer, ValidateCouponTokenizer>(service: "subscription", action: "validateCoupon")
+			.setParam(key: "id", value: id)
+			.setParam(key: "code", value: code)
+
+		return request
+	}
+}
