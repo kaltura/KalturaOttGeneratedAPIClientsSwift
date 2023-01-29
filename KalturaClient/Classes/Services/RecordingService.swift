dfd5de2fc1640d64930ec072fbdf554b1a1fdf93
Backend CI <ott.rnd.core@kaltura.com>
2023-01-29 09:02:08 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs-ios-and-android/546, branch 8_4_1_2

---
+++ KalturaClient/Classes/Services/RecordingService.swift
@@ -0,0 +1,255 @@
+// ===================================================================================================
+//                           _  __     _ _
+//                          | |/ /__ _| | |_ _  _ _ _ __ _
+//                          | ' </ _` | |  _| || | '_/ _` |
+//                          |_|\_\__,_|_|\__|\_,_|_| \__,_|
+//
+// This file is part of the Kaltura Collaborative Media Suite which allows users
+// to do with audio, video, and animation what Wiki platfroms allow them to do with
+// text.
+//
+// Copyright (C) 2006-2020  Kaltura Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// @ignore
+// ===================================================================================================
+
+/**
+ * This class was generated using exec.php
+ * against an XML schema provided by Kaltura.
+ * 
+ * MANUAL CHANGES TO THIS CLASS WILL BE OVERWRITTEN.
+ */
+
+public final class RecordingService{
+
+	public class AddTokenizer: ClientTokenizer  {
+		
+		public func recording<T: Recording.RecordingTokenizer>() -> T {
+			return T(self.append("recording"))
+		}
+	}
+
+	/**  Issue a record request for a program  */
+	public static func add(recording: Recording) -> RequestBuilder<Recording, Recording.RecordingTokenizer, AddTokenizer> {
+		let request: RequestBuilder<Recording, Recording.RecordingTokenizer, AddTokenizer> = RequestBuilder<Recording, Recording.RecordingTokenizer, AddTokenizer>(service: "recording", action: "add")
+			.setParam(key: "recording", value: recording)
+
+		return request
+	}
+
+	public class BulkdeleteTokenizer: ClientTokenizer  {
+		
+		public var recordingIds: BaseTokenizedObject {
+			get {
+				return self.append("recordingIds") 
+			}
+		}
+	}
+
+	/**  Delete list of user&amp;#39;s recordings. Recording can be deleted only in
+	  status Recorded.              Possible error codes for each recording:
+	  RecordingNotFound = 3039, RecordingStatusNotValid = 3043, Error = 1  */
+	public static func bulkdelete(recordingIds: String) -> ArrayRequestBuilder<ActionResult, ArrayTokenizedObject<ActionResult.ActionResultTokenizer>, BulkdeleteTokenizer> {
+		let request: ArrayRequestBuilder<ActionResult, ArrayTokenizedObject<ActionResult.ActionResultTokenizer>, BulkdeleteTokenizer> = ArrayRequestBuilder<ActionResult, ArrayTokenizedObject<ActionResult.ActionResultTokenizer>, BulkdeleteTokenizer>(service: "recording", action: "bulkdelete")
+			.setParam(key: "recordingIds", value: recordingIds)
+
+		return request
+	}
+
+	public class CancelTokenizer: ClientTokenizer  {
+		
+		public var id: BaseTokenizedObject {
+			get {
+				return self.append("id") 
+			}
+		}
+	}
+
+	/**  Cancel a previously requested recording. Cancel recording can be called for
+	  recording in status Scheduled or Recording Only  */
+	public static func cancel(id: Int64) -> RequestBuilder<Recording, Recording.RecordingTokenizer, CancelTokenizer> {
+		let request: RequestBuilder<Recording, Recording.RecordingTokenizer, CancelTokenizer> = RequestBuilder<Recording, Recording.RecordingTokenizer, CancelTokenizer>(service: "recording", action: "cancel")
+			.setParam(key: "id", value: id)
+
+		return request
+	}
+
+	public class DeleteTokenizer: ClientTokenizer  {
+		
+		public var id: BaseTokenizedObject {
+			get {
+				return self.append("id") 
+			}
+		}
+	}
+
+	/**  Delete one or more user recording(s). Delete recording can be called only for
+	  recordings in status Recorded  */
+	public static func delete(id: Int64) -> RequestBuilder<Recording, Recording.RecordingTokenizer, DeleteTokenizer> {
+		let request: RequestBuilder<Recording, Recording.RecordingTokenizer, DeleteTokenizer> = RequestBuilder<Recording, Recording.RecordingTokenizer, DeleteTokenizer>(service: "recording", action: "delete")
+			.setParam(key: "id", value: id)
+
+		return request
+	}
+
+	public class GetTokenizer: ClientTokenizer  {
+		
+		public var id: BaseTokenizedObject {
+			get {
+				return self.append("id") 
+			}
+		}
+	}
+
+	/**  Returns recording object by internal identifier  */
+	public static func get(id: Int64) -> RequestBuilder<Recording, Recording.RecordingTokenizer, GetTokenizer> {
+		let request: RequestBuilder<Recording, Recording.RecordingTokenizer, GetTokenizer> = RequestBuilder<Recording, Recording.RecordingTokenizer, GetTokenizer>(service: "recording", action: "get")
+			.setParam(key: "id", value: id)
+
+		return request
+	}
+
+	public class ImmediateRecordTokenizer: ClientTokenizer  {
+		
+		public var assetId: BaseTokenizedObject {
+			get {
+				return self.append("assetId") 
+			}
+		}
+		
+		public var epgChannelId: BaseTokenizedObject {
+			get {
+				return self.append("epgChannelId") 
+			}
+		}
+		
+		public var endPadding: BaseTokenizedObject {
+			get {
+				return self.append("endPadding") 
+			}
+		}
+	}
+
+	/**  Immediate Record  */
+	public static func immediateRecord(assetId: Int64, epgChannelId: Int64, endPadding: Int) -> RequestBuilder<ImmediateRecording, ImmediateRecording.ImmediateRecordingTokenizer, ImmediateRecordTokenizer> {
+		let request: RequestBuilder<ImmediateRecording, ImmediateRecording.ImmediateRecordingTokenizer, ImmediateRecordTokenizer> = RequestBuilder<ImmediateRecording, ImmediateRecording.ImmediateRecordingTokenizer, ImmediateRecordTokenizer>(service: "recording", action: "immediateRecord")
+			.setParam(key: "assetId", value: assetId)
+			.setParam(key: "epgChannelId", value: epgChannelId)
+			.setParam(key: "endPadding", value: endPadding)
+
+		return request
+	}
+
+	public class ListTokenizer: ClientTokenizer  {
+		
+		public func filter<T: RecordingFilter.RecordingFilterTokenizer>() -> T {
+			return T(self.append("filter"))
+		}
+		
+		public func pager<T: FilterPager.FilterPagerTokenizer>() -> T {
+			return T(self.append("pager"))
+		}
+	}
+
+	public static func list() -> RequestBuilder<RecordingListResponse, RecordingListResponse.RecordingListResponseTokenizer, ListTokenizer> {
+		return list(filter: nil)
+	}
+
+	public static func list(filter: RecordingFilter?) -> RequestBuilder<RecordingListResponse, RecordingListResponse.RecordingListResponseTokenizer, ListTokenizer> {
+		return list(filter: filter, pager: nil)
+	}
+
+	/**  Return a list of recordings for the household with optional filter by status and
+	  KSQL.  */
+	public static func list(filter: RecordingFilter?, pager: FilterPager?) -> RequestBuilder<RecordingListResponse, RecordingListResponse.RecordingListResponseTokenizer, ListTokenizer> {
+		let request: RequestBuilder<RecordingListResponse, RecordingListResponse.RecordingListResponseTokenizer, ListTokenizer> = RequestBuilder<RecordingListResponse, RecordingListResponse.RecordingListResponseTokenizer, ListTokenizer>(service: "recording", action: "list")
+			.setParam(key: "filter", value: filter)
+			.setParam(key: "pager", value: pager)
+
+		return request
+	}
+
+	public class ProtectTokenizer: ClientTokenizer  {
+		
+		public var id: BaseTokenizedObject {
+			get {
+				return self.append("id") 
+			}
+		}
+	}
+
+	/**  Deprecated, please use recording.update instead              Protects an
+	  existing recording from the cleanup process for the defined protection period  */
+	public static func protect(id: Int64) -> RequestBuilder<Recording, Recording.RecordingTokenizer, ProtectTokenizer> {
+		let request: RequestBuilder<Recording, Recording.RecordingTokenizer, ProtectTokenizer> = RequestBuilder<Recording, Recording.RecordingTokenizer, ProtectTokenizer>(service: "recording", action: "protect")
+			.setParam(key: "id", value: id)
+
+		return request
+	}
+
+	public class StopTokenizer: ClientTokenizer  {
+		
+		public var assetId: BaseTokenizedObject {
+			get {
+				return self.append("assetId") 
+			}
+		}
+		
+		public var epgChannelId: BaseTokenizedObject {
+			get {
+				return self.append("epgChannelId") 
+			}
+		}
+		
+		public var householdRecordingId: BaseTokenizedObject {
+			get {
+				return self.append("householdRecordingId") 
+			}
+		}
+	}
+
+	/**  Stop ongoing household recording  */
+	public static func stop(assetId: Int64, epgChannelId: Int64, householdRecordingId: Int64) -> RequestBuilder<Recording, Recording.RecordingTokenizer, StopTokenizer> {
+		let request: RequestBuilder<Recording, Recording.RecordingTokenizer, StopTokenizer> = RequestBuilder<Recording, Recording.RecordingTokenizer, StopTokenizer>(service: "recording", action: "stop")
+			.setParam(key: "assetId", value: assetId)
+			.setParam(key: "epgChannelId", value: epgChannelId)
+			.setParam(key: "householdRecordingId", value: householdRecordingId)
+
+		return request
+	}
+
+	public class UpdateTokenizer: ClientTokenizer  {
+		
+		public var id: BaseTokenizedObject {
+			get {
+				return self.append("id") 
+			}
+		}
+		
+		public func recording<T: Recording.RecordingTokenizer>() -> T {
+			return T(self.append("recording"))
+		}
+	}
+
+	/**  Update an existing recording with is protected field  */
+	public static func update(id: Int64, recording: Recording) -> RequestBuilder<Recording, Recording.RecordingTokenizer, UpdateTokenizer> {
+		let request: RequestBuilder<Recording, Recording.RecordingTokenizer, UpdateTokenizer> = RequestBuilder<Recording, Recording.RecordingTokenizer, UpdateTokenizer>(service: "recording", action: "update")
+			.setParam(key: "id", value: id)
+			.setParam(key: "recording", value: recording)
+
+		return request
+	}
+}
