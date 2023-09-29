f39cd7b4e06b7064b5f1946f7ed8f0c2edd5d546
Backend CI <ott.rnd.core@kaltura.com>
2023-09-29 09:09:46 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs-ios-and-android/604, branch 9.2.1.3

---
+++ KalturaClient/Classes/Services/PurchaseSettingsService.swift
@@ -0,0 +1,77 @@
+// ===================================================================================================
+//                           _  __     _ _
+//                          | |/ /__ _| | |_ _  _ _ _ __ _
+//                          | ' </ _` | |  _| || | '_/ _` |
+//                          |_|\_\__,_|_|\__|\_,_|_| \__,_|
+//
+// This file is part of the Kaltura Collaborative Media Suite which allows users
+// to do with audio, video, and animation what Wiki platfroms allow them to do with
+// text.
+//
+// Copyright (C) 2006-2020  Kaltura Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// @ignore
+// ===================================================================================================
+
+/**
+ * This class was generated using exec.php
+ * against an XML schema provided by Kaltura.
+ * 
+ * MANUAL CHANGES TO THIS CLASS WILL BE OVERWRITTEN.
+ */
+
+public final class PurchaseSettingsService{
+
+	public class GetTokenizer: ClientTokenizer  {
+		
+		public var by: BaseTokenizedObject {
+			get {
+				return self.append("by") 
+			}
+		}
+	}
+
+	/**  Retrieve the purchase settings.              Includes specification of where
+	  these settings were defined – account, household or user  */
+	public static func get(by: EntityReferenceBy) -> RequestBuilder<PurchaseSettings, PurchaseSettings.PurchaseSettingsTokenizer, GetTokenizer> {
+		let request: RequestBuilder<PurchaseSettings, PurchaseSettings.PurchaseSettingsTokenizer, GetTokenizer> = RequestBuilder<PurchaseSettings, PurchaseSettings.PurchaseSettingsTokenizer, GetTokenizer>(service: "purchasesettings", action: "get")
+			.setParam(key: "by", value: by.rawValue)
+
+		return request
+	}
+
+	public class UpdateTokenizer: ClientTokenizer  {
+		
+		public var entityReference: BaseTokenizedObject {
+			get {
+				return self.append("entityReference") 
+			}
+		}
+		
+		public func settings<T: PurchaseSettings.PurchaseSettingsTokenizer>() -> T {
+			return T(self.append("settings"))
+		}
+	}
+
+	/**  Set a purchase PIN for the household or user  */
+	public static func update(entityReference: EntityReferenceBy, settings: PurchaseSettings) -> RequestBuilder<PurchaseSettings, PurchaseSettings.PurchaseSettingsTokenizer, UpdateTokenizer> {
+		let request: RequestBuilder<PurchaseSettings, PurchaseSettings.PurchaseSettingsTokenizer, UpdateTokenizer> = RequestBuilder<PurchaseSettings, PurchaseSettings.PurchaseSettingsTokenizer, UpdateTokenizer>(service: "purchasesettings", action: "update")
+			.setParam(key: "entityReference", value: entityReference.rawValue)
+			.setParam(key: "settings", value: settings)
+
+		return request
+	}
+}
