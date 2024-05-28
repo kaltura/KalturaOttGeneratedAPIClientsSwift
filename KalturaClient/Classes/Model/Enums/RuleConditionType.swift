7ad26740963e17f1fca6e2c05059e39846c0888f
Backend CI <ott.rnd.core@kaltura.com>
2024-05-28 16:00:52 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs-ios-and-android/665, branch 9.9.0.4

---
+++ KalturaClient/Classes/Model/Enums/RuleConditionType.swift
@@ -0,0 +1,59 @@
+// ===================================================================================================
+//                           _  __     _ _
+//                          | |/ /__ _| | |_ _  _ _ _ __ _
+//                          | ' </ _` | |  _| || | '_/ _` |
+//                          |_|\_\__,_|_|\__|\_,_|_| \__,_|
+//
+// This file is part of the Kaltura Collaborative Media Suite which allows users
+// to do with audio, video, and animation what Wiki platfroms allow them to do with
+// text.
+//
+// Copyright (C) 2006-2020  Kaltura Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// @ignore
+// ===================================================================================================
+/**
+ * This class was generated using exec.php
+ * against an XML schema provided by Kaltura.
+ * 
+ * MANUAL CHANGES TO THIS CLASS WILL BE OVERWRITTEN.
+ */
+public enum RuleConditionType: String {
+	case ASSET = "ASSET"
+	case COUNTRY = "COUNTRY"
+	case CONCURRENCY = "CONCURRENCY"
+	case IP_RANGE = "IP_RANGE"
+	case BUSINESS_MODULE = "BUSINESS_MODULE"
+	case SEGMENTS = "SEGMENTS"
+	case DATE = "DATE"
+	case OR = "OR"
+	case HEADER = "HEADER"
+	case USER_SUBSCRIPTION = "USER_SUBSCRIPTION"
+	case ASSET_SUBSCRIPTION = "ASSET_SUBSCRIPTION"
+	case USER_ROLE = "USER_ROLE"
+	case DEVICE_BRAND = "DEVICE_BRAND"
+	case DEVICE_FAMILY = "DEVICE_FAMILY"
+	case DEVICE_MANUFACTURER = "DEVICE_MANUFACTURER"
+	case DEVICE_MODEL = "DEVICE_MODEL"
+	case DEVICE_UDID_DYNAMIC_LIST = "DEVICE_UDID_DYNAMIC_LIST"
+	case DYNAMIC_KEYS = "DYNAMIC_KEYS"
+	case USER_SESSION_PROFILE = "USER_SESSION_PROFILE"
+	case DEVICE_DYNAMIC_DATA = "DEVICE_DYNAMIC_DATA"
+	case IP_V6_RANGE = "IP_V6_RANGE"
+	case ASSET_SHOP = "ASSET_SHOP"
+	case CHANNEL = "CHANNEL"
+	case FILE_TYPE = "FILE_TYPE"
+}
---
+++ KalturaClient/Classes/Model/Objects/RelatedEntity.swift
@@ -0,0 +1,90 @@
+// ===================================================================================================
+//                           _  __     _ _
+//                          | |/ /__ _| | |_ _  _ _ _ __ _
+//                          | ' </ _` | |  _| || | '_/ _` |
+//                          |_|\_\__,_|_|\__|\_,_|_| \__,_|
+//
+// This file is part of the Kaltura Collaborative Media Suite which allows users
+// to do with audio, video, and animation what Wiki platfroms allow them to do with
+// text.
+//
+// Copyright (C) 2006-2020  Kaltura Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// @ignore
+// ===================================================================================================
+
+/**
+ * This class was generated using exec.php
+ * against an XML schema provided by Kaltura.
+ * 
+ * MANUAL CHANGES TO THIS CLASS WILL BE OVERWRITTEN.
+ */
+
+open class RelatedEntity: ObjectBase {
+
+	public class RelatedEntityTokenizer: ObjectBase.ObjectBaseTokenizer {
+		
+		public var id: BaseTokenizedObject {
+			get {
+				return self.append("id") 
+			}
+		}
+		
+		public var type: BaseTokenizedObject {
+			get {
+				return self.append("type") 
+			}
+		}
+	}
+
+	/**  Unique identifier for the related entry  */
+	public var id: String? = nil
+	/**  Defines related entry type  */
+	public var type: RelatedEntityType? = nil
+
+
+	public func setMultiRequestToken(id: String) {
+		self.dict["id"] = id
+	}
+	
+	public func setMultiRequestToken(type: String) {
+		self.dict["type"] = type
+	}
+	
+	public override func populate(_ dict: [String: Any]) throws {
+		try super.populate(dict);
+		// set members values:
+		if dict["id"] != nil {
+			id = dict["id"] as? String
+		}
+		if dict["type"] != nil {
+			type = RelatedEntityType(rawValue: "\(dict["type"]!)")
+		}
+
+	}
+
+	internal override func toDictionary() -> [String: Any] {
+		var dict: [String: Any] = super.toDictionary()
+		if(id != nil) {
+			dict["id"] = id!
+		}
+		if(type != nil) {
+			dict["type"] = type!.rawValue
+		}
+		return dict
+	}
+}
+
