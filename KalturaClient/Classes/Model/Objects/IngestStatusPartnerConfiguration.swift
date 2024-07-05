47ce2f7c1d9f1df2d6283181139f4e2b8a2d0e81
Backend CI <ott.rnd.core@kaltura.com>
2024-07-05 09:59:02 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs-ios-and-android/672, branch 10.2.0.1

---
+++ KalturaClient/Classes/Model/Objects/IngestStatusPartnerConfiguration.swift
@@ -0,0 +1,76 @@
+// ===================================================================================================
+//                           _  __     _ _
+//                          | |/ /__ _| | |_ _  _ _ _ __ _
+//                          | ' </ _` | |  _| || | '_/ _` |
+//                          |_|\_\__,_|_|\__|\_,_|_| \__,_|
+//
+// This file is part of the Kaltura Collaborative Media Suite which allows users
+// to do with audio, video, and animation what Wiki platfroms allow them to do with
+// text.
+//
+// Copyright (C) 2006-2020  Kaltura Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// @ignore
+// ===================================================================================================
+
+/**
+ * This class was generated using exec.php
+ * against an XML schema provided by Kaltura.
+ * 
+ * MANUAL CHANGES TO THIS CLASS WILL BE OVERWRITTEN.
+ */
+
+open class IngestStatusPartnerConfiguration: ObjectBase {
+
+	public class IngestStatusPartnerConfigurationTokenizer: ObjectBase.ObjectBaseTokenizer {
+		
+		public func epg<T: IngestStatusEpgConfiguration.IngestStatusEpgConfigurationTokenizer>() -> T {
+			return T(self.append("epg"))
+		}
+		
+		public func vod<T: IngestStatusVodConfiguration.IngestStatusVodConfigurationTokenizer>() -> T {
+			return T(self.append("vod"))
+		}
+	}
+
+	/**  Defines the epg configuration of the partner.  */
+	public var epg: IngestStatusEpgConfiguration? = nil
+	/**  Defines the vod configuration of the partner.  */
+	public var vod: IngestStatusVodConfiguration? = nil
+
+
+	public override func populate(_ dict: [String: Any]) throws {
+		try super.populate(dict);
+		// set members values:
+		if dict["epg"] != nil {
+		epg = try JSONParser.parse(object: dict["epg"] as! [String: Any])		}
+		if dict["vod"] != nil {
+		vod = try JSONParser.parse(object: dict["vod"] as! [String: Any])		}
+
+	}
+
+	internal override func toDictionary() -> [String: Any] {
+		var dict: [String: Any] = super.toDictionary()
+		if(epg != nil) {
+			dict["epg"] = epg!.toDictionary()
+		}
+		if(vod != nil) {
+			dict["vod"] = vod!.toDictionary()
+		}
+		return dict
+	}
+}
+
