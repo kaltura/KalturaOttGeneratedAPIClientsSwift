28d8dd48a0ba8ef01f6c2733f9ed3807e738313c
Backend CI <ott.rnd.core@kaltura.com>
2024-12-24 22:03:58 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs-ios-and-android/711, branch 10.4.1.4

---
+++ KalturaClient/Classes/Model/Objects/CollectionCouponGroup.swift
@@ -0,0 +1,109 @@
+// ===================================================================================================
+//                           _  __     _ _
+//                          | |/ /__ _| | |_ _  _ _ _ __ _
+//                          | ' </ _` | |  _| || | '_/ _` |
+//                          |_|\_\__,_|_|\__|\_,_|_| \__,_|
+//
+// This file is part of the Kaltura Collaborative Media Suite which allows users
+// to do with audio, video, and animation what Wiki platfroms allow them to do with
+// text.
+//
+// Copyright (C) 2006-2020  Kaltura Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// @ignore
+// ===================================================================================================
+
+/**
+ * This class was generated using exec.php
+ * against an XML schema provided by Kaltura.
+ * 
+ * MANUAL CHANGES TO THIS CLASS WILL BE OVERWRITTEN.
+ */
+
+/**  Coupons group details  */
+open class CollectionCouponGroup: ObjectBase {
+
+	public class CollectionCouponGroupTokenizer: ObjectBase.ObjectBaseTokenizer {
+		
+		public var id: BaseTokenizedObject {
+			get {
+				return self.append("id") 
+			}
+		}
+		
+		public var startDate: BaseTokenizedObject {
+			get {
+				return self.append("startDate") 
+			}
+		}
+		
+		public var endDate: BaseTokenizedObject {
+			get {
+				return self.append("endDate") 
+			}
+		}
+	}
+
+	/**  Coupon group identifier  */
+	public var id: Int64? = nil
+	/**  The first date the coupons in this coupons group are valid  */
+	public var startDate: Int64? = nil
+	/**  The last date the coupons in this coupons group are valid  */
+	public var endDate: Int64? = nil
+
+
+	public func setMultiRequestToken(id: String) {
+		self.dict["id"] = id
+	}
+	
+	public func setMultiRequestToken(startDate: String) {
+		self.dict["startDate"] = startDate
+	}
+	
+	public func setMultiRequestToken(endDate: String) {
+		self.dict["endDate"] = endDate
+	}
+	
+	public override func populate(_ dict: [String: Any]) throws {
+		try super.populate(dict);
+		// set members values:
+		if dict["id"] != nil {
+			id = Int64("\(dict["id"]!)")
+		}
+		if dict["startDate"] != nil {
+			startDate = Int64("\(dict["startDate"]!)")
+		}
+		if dict["endDate"] != nil {
+			endDate = Int64("\(dict["endDate"]!)")
+		}
+
+	}
+
+	internal override func toDictionary() -> [String: Any] {
+		var dict: [String: Any] = super.toDictionary()
+		if(id != nil) {
+			dict["id"] = id!
+		}
+		if(startDate != nil) {
+			dict["startDate"] = startDate!
+		}
+		if(endDate != nil) {
+			dict["endDate"] = endDate!
+		}
+		return dict
+	}
+}
+
