6cf4d2a00c860d034a637274eff13f087869054e
Backend CI <ott.rnd.core@kaltura.com>
2025-08-06 09:10:46 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs-ios-and-android/766, branch 11.5.0.1

---
+++ KalturaClient/Classes/Model/Objects/CollectionFilter.swift
@@ -0,0 +1,164 @@
+// ===================================================================================================
+//                           _  __     _ _
+//                          | |/ /__ _| | |_ _  _ _ _ __ _
+//                          | ' </ _` | |  _| || | '_/ _` |
+//                          |_|\_\__,_|_|\__|\_,_|_| \__,_|
+//
+// This file is part of the Kaltura Collaborative Media Suite which allows users
+// to do with audio, video, and animation what Wiki platfroms allow them to do with
+// text.
+//
+// Copyright (C) 2006-2020  Kaltura Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// @ignore
+// ===================================================================================================
+
+/**
+ * This class was generated using exec.php
+ * against an XML schema provided by Kaltura.
+ * 
+ * MANUAL CHANGES TO THIS CLASS WILL BE OVERWRITTEN.
+ */
+
+/**  Collection Filter  */
+open class CollectionFilter: Filter {
+
+	public class CollectionFilterTokenizer: Filter.FilterTokenizer {
+		
+		public var collectionIdIn: BaseTokenizedObject {
+			get {
+				return self.append("collectionIdIn") 
+			}
+		}
+		
+		public var mediaFileIdEqual: BaseTokenizedObject {
+			get {
+				return self.append("mediaFileIdEqual") 
+			}
+		}
+		
+		public var couponGroupIdEqual: BaseTokenizedObject {
+			get {
+				return self.append("couponGroupIdEqual") 
+			}
+		}
+		
+		public var alsoInactive: BaseTokenizedObject {
+			get {
+				return self.append("alsoInactive") 
+			}
+		}
+		
+		public var assetUserRuleIdIn: BaseTokenizedObject {
+			get {
+				return self.append("assetUserRuleIdIn") 
+			}
+		}
+		
+		public var nameContains: BaseTokenizedObject {
+			get {
+				return self.append("nameContains") 
+			}
+		}
+	}
+
+	/**  Comma separated collection IDs  */
+	public var collectionIdIn: String? = nil
+	/**  Media-file ID to get the collections by  */
+	public var mediaFileIdEqual: Int? = nil
+	/**  couponGroupIdEqual  */
+	public var couponGroupIdEqual: Int? = nil
+	/**  return also inactive  */
+	public var alsoInactive: Bool? = nil
+	/**  comma-separated list of KalturaCollection.assetUserRuleId values.  Matching
+	  KalturaCollection objects will be returned by the filter.  */
+	public var assetUserRuleIdIn: String? = nil
+	/**  A string that is included in the collection name  */
+	public var nameContains: String? = nil
+
+
+	public func setMultiRequestToken(collectionIdIn: String) {
+		self.dict["collectionIdIn"] = collectionIdIn
+	}
+	
+	public func setMultiRequestToken(mediaFileIdEqual: String) {
+		self.dict["mediaFileIdEqual"] = mediaFileIdEqual
+	}
+	
+	public func setMultiRequestToken(couponGroupIdEqual: String) {
+		self.dict["couponGroupIdEqual"] = couponGroupIdEqual
+	}
+	
+	public func setMultiRequestToken(alsoInactive: String) {
+		self.dict["alsoInactive"] = alsoInactive
+	}
+	
+	public func setMultiRequestToken(assetUserRuleIdIn: String) {
+		self.dict["assetUserRuleIdIn"] = assetUserRuleIdIn
+	}
+	
+	public func setMultiRequestToken(nameContains: String) {
+		self.dict["nameContains"] = nameContains
+	}
+	
+	public override func populate(_ dict: [String: Any]) throws {
+		try super.populate(dict);
+		// set members values:
+		if dict["collectionIdIn"] != nil {
+			collectionIdIn = dict["collectionIdIn"] as? String
+		}
+		if dict["mediaFileIdEqual"] != nil {
+			mediaFileIdEqual = dict["mediaFileIdEqual"] as? Int
+		}
+		if dict["couponGroupIdEqual"] != nil {
+			couponGroupIdEqual = dict["couponGroupIdEqual"] as? Int
+		}
+		if dict["alsoInactive"] != nil {
+			alsoInactive = dict["alsoInactive"] as? Bool
+		}
+		if dict["assetUserRuleIdIn"] != nil {
+			assetUserRuleIdIn = dict["assetUserRuleIdIn"] as? String
+		}
+		if dict["nameContains"] != nil {
+			nameContains = dict["nameContains"] as? String
+		}
+
+	}
+
+	internal override func toDictionary() -> [String: Any] {
+		var dict: [String: Any] = super.toDictionary()
+		if(collectionIdIn != nil) {
+			dict["collectionIdIn"] = collectionIdIn!
+		}
+		if(mediaFileIdEqual != nil) {
+			dict["mediaFileIdEqual"] = mediaFileIdEqual!
+		}
+		if(couponGroupIdEqual != nil) {
+			dict["couponGroupIdEqual"] = couponGroupIdEqual!
+		}
+		if(alsoInactive != nil) {
+			dict["alsoInactive"] = alsoInactive!
+		}
+		if(assetUserRuleIdIn != nil) {
+			dict["assetUserRuleIdIn"] = assetUserRuleIdIn!
+		}
+		if(nameContains != nil) {
+			dict["nameContains"] = nameContains!
+		}
+		return dict
+	}
+}
+
